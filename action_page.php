922b65397482a43be82046117571372cb04624e7
Cristian Lepore <clepore@DESKTOP-8O4GEQ5.home>
2020-10-07 14:58:44 +0100

    .

--- action_page.php
+++ action_page.php
@@ -12,11 +12,11 @@ fclose($file);
 
 // Use ls command to shell_exec 
 // function 
-$output = shell_exec('javac My_run.java'); 
-$output = shell_exec('java My_run'); 
+$command = "javac -verbose prova.java 2>&1";
+$result = passthru($command);
 
 // Display the list of all file 
 // and directory
-echo "<pre>$output</pre>"; 
+echo $result;
 
 ?>
