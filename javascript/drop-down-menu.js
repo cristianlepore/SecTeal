06e4f6f7c52fa17a982afdbbbd6c0d00f76eb8f5
Cristian Lepore <clepore@DESKTOP-8O4GEQ5.home>
2020-10-18 15:47:24 +0100

    eample finished

--- javascript/drop-down-menu.js
+++ javascript/drop-down-menu.js
@@ -4,10 +4,32 @@ function dropDownMenu() {
 
     var horizontal_line = "<hr style='height: 1px; border-width: 0; color: gray; background-color: gray'>";
 
-    var example1_text = "";
-    var example1_title = "";
-    var example_code = "";
+    // Send transaction
+    var sendTxn_text = "This is a first simple example. The scenario shows a check on the receiver address.";
+    var sendTxn_title = "<b>" + "Send Transaction:" + "</b><br>";
+    var sendTxn_code = "(tx(0).rcv = addr ZZAF5ARA4MEC5PVDOP64JM5O5MQST63Q2KOY2FLYFLXXD3PFSNJJBYAFZM)" + "<br>";
 
+
+    // Close account
+    var closeAccount_text = "This is a simple example. The sample describes a quite common scenario. First it checks that the transacion fee is less that 0,001 Algos, then it sends Algos to the receiver and sets the close reminder field to close the account and withdraw the deposit from it.";
+    var closeAccount_title = "<b>" + "Close Account:" + "</b><br>";
+    var closeAccount_code = "(" + "<br>" +
+        "&nbsp&nbsp&nbsp&nbsp&nbsp" + "(tx(0).fee < int 1000)" + "<br>" +
+        "&nbsp&nbsp&nbsp&nbsp&nbsp" + "(" + "<br>" +
+        "&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp" + "(tx(0).rcv = addr ZZAF5ARA4MEC5PVDOP64JM5O5MQST63Q2KOY2FLYFLXXD3PFSNJJBYAFZM)" + "<br>" +
+        "&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp" + "and" + "<br>" +
+        "&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp" + "(tx(0).crcv = addr ZZAF5ARA4MEC5PVDOP64JM5O5MQST63Q2KOY2FLYFLXXD3PFSNJJBYAFZM)" + "<br>" +
+        "&nbsp&nbsp&nbsp&nbsp&nbsp" + ")" + "<br>" +
+        ")" + "<br>";
+
+
+    // Versig
+    var versig_text = "The script versig(e1,e2,e3) verifies a signature e2 on the message obtained by concatenating the enclosing script and e1, using public key e3." + "<br>";
+    var versig_title = "<b>" + "Versig:" + "</b><br>";
+    var versig_code = "verisig(arg(0),arg(1),addr ZZAF5ARA4MEC5PVDOP64JM5O5MQST63Q2KOY2FLYFLXXD3PFSNJJBYAFZM)" + "<br>";
+
+
+    // Oracle
     var oracle_text = "<b>" + "Oracle" + "</b>" + ". Either " + "<i>" + "a " + "</i>" + "or " + "<i>" + "b " + "</i>" + "to withdraw all the Algos in the contract, depending on the outcome of a certain boolean event, certified by an oracle " + "<i>" + "o " + "</i>" + "by signing the value 1 or 0, respectively. The contract only accepts close txs, " + "<i>" + "arg(0) " + "</i>" + "is the event's outcome, " + "<i>" + "arg(1) " + "</i>" + "is " + "<i>" + "o" + "</i>" + "’s signature on (" + "<i>" + "Oracle" + "</i>" + "," + "<i>" + "arg(0)" + "</i>" + "). After round r" + "<sub>" + "max" + "</sub>" + "   , a  can collect the money.";
     var oracle_title = "<b>" + "Oracle:" + "</b><br>";
     var oracle_code = "(" + "<br>" +
@@ -36,15 +58,26 @@ function dropDownMenu() {
         "&nbsp&nbsp&nbsp&nbsp&nbsp" + ")" + "<br>" +
         ")" + "<br>";
 
+
+    // HTLC
     var htlc_text = "<b>" + "Hash Time Lock Contract (HTLC)" + "</b>" + ". A user " + "<i>" + "a " + "</i> " + "promises that she will either reveal a secret s" + " <sub>" + "a" + "</sub>" + " by round r" + "<sub>" + "max" + "</sub>" + ", or pay a penalty to " + " <i> " + "b" + "</i>" + ". More sophisticated contracts, e.g. gambling games, use this mechanism to let players generate random numbers in a fair way. We define the HTLC as the following contract, parameterised on the two users " + " <i> " + "a" + "</i>" + ", " + " <i>" + " b " + "</i> " + "and the hash h" + "<sub>" + "a" + "</sub>" + " = H(s" + "<sub>" + "a" + "</sub>" + ") of the secret.";
     var htlc_title = "HTLC";
     var htlc_code = "";
     var htlc_conclusion = "The contract accepts only close transactions with receiver " + "<i>" + "a " + "</i>" + "or " + "<i>" + "b" + "</i>" + ". User " + "<i>" + "a " + "</i>" + "can collect the funds in the contract only by providing the secret s" + "<sub>" + "a " + "</sub>" + "in " + "<i>" + "arg(0)" + "</i>" + ", effectively making s" + "<sub>" + "a " + "</sub>" + "public. Instead, if " + "<i>" + "a " + "</i>" + "does not reveal s" + "<sub>" + "a " + "</sub>" + ", then " + "<i>" + "b " + "</i>" + "can collect the funds after round r" + "<sub>" + "max" + "</sub>" + ".";
 
+
     // Check the correct example
-    if (value === 'Example1') {
-        document.getElementById("description").innerHTML = example1;
-    } else if (value === 'Oracle') {
+    if (value === 'sendTxn') {
+        sendTxn = horizontal_line + sendTxn_text + "<p class='vertical_space-half'></p>" + sendTxn_title + "<br>" + sendTxn_code;
+        document.getElementById("description").innerHTML = sendTxn;
+    } else if (value === 'closeAccount') {
+        closeAccount = horizontal_line + closeAccount_text + "<p class='vertical_space-half'></p>" + closeAccount_title + "<br>" + closeAccount_code;
+        document.getElementById("description").innerHTML = closeAccount;
+    } else if (value === 'versig') {
+        versig = horizontal_line + versig_text + "<p class='vertical_space-half'></p>" + versig_title + "<br>" + versig_code;
+        document.getElementById("description").innerHTML = versig;
+    }
+    else if (value === 'Oracle') {
         oracle = horizontal_line + oracle_text + "<p class='vertical_space-half'></p>" + oracle_title + "<br>" + oracle_code;
         document.getElementById("description").innerHTML = oracle;
     } else if (value === 'htlc') {
